4e79e838fb23f6ae2f85a1541474804d9b7cddd2
James Costa <130270571+JAMESCOSTA01@users.noreply.github.com>
2023-04-14 16:35:23 -0300

    Add files via upload

---
+++ Projeto teste Juntar Nome.java
@@ -0,0 +1,39 @@
+public class MinhaClasse { 
+
+ 
+ 
+
+public static void main (String [] args) { 
+
+ 
+ 
+
+    String primeiroNome = "James"; 
+
+    String segundoNome = "Costa"; 
+
+ 
+ 
+
+    String nomeCompleto = nomeCompleto (primeiroNome,segundoNome); 
+
+ 
+ 
+
+    System.out.println(nomeCompleto); 
+
+} 
+
+ 
+ 
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) { 
+
+    return "Resultado do Método " + primeiroNome.concat(" ").concat(segundoNome); 
+
+} 
+
+ 
+ 
+
+} 
